c1dcd130637110a1b299845e67a2c288b380db69
bhytzer1 <davidemandricardo@tuta.com>
2025-12-01 17:30:45 +0100

    aggiornamento bug

---
+++ ft_strlcat.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmandric <dmandric@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/01 12:08:27 by dmandric          #+#    #+#             */
+/*   Updated: 2025/12/01 17:17:57 by dmandric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "libft.h"
+# include <stddef.h>
+
+size_t  ft_strlcat(char *dest, const char *src, size_t dest_size)
+{
+    size_t dest_len;
+    size_t src_len;
+    size_t counter;
+
+    dest_len = 0;
+    while(dest[dest_len])
+    {
+        dest_len++;
+    }
+    
+    src_len = 0;
+    while(src[src_len] && src_len < dest_size)
+    {
+        src_len++;
+    }
+    
+    if(dest_len == dest_size)
+    {
+        return(dest_size + src_len);
+    }
+
+    counter = 0;
+    while(src[counter] && (dest_len + counter + 1) < dest_size)
+    {  
+        dest[dest_len + counter] = src[counter];
+        counter++;
+    }
+
+    if (dest_len < dest_size)
+    {
+        dest[dest_size + counter] = '\0';
+    }
+    
+    return(dest_len + src_len);
+    
+}
